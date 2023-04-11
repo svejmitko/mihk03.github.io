a8fc13d4bbebe07d9c82352c59d63ce64f5a926c
mihk03 <93489799+mihk03@users.noreply.github.com>
2023-04-11 14:49:49 +0300

    Update aframe-ar.js

--- aframe-ar.js
+++ aframe-ar.js
@@ -5933,6 +5933,12 @@ ARjs.Source.prototype._initSourceWebcam = function(onReady, onError) {
         }
       }
     }
+    var backCam2 = devices.filter(d=>{
+      return d.label && d.label == "camera2 0, facing back";
+  })
+  if (backCam2.length) {
+      userMediaConstraints.video.deviceId = backCam2[0].deviceId
+  }
     // get a device which satisfy the constraints
     navigator.mediaDevices.getUserMedia(userMediaConstraints).then(function success(stream) {
       // set the .src of the domElement
